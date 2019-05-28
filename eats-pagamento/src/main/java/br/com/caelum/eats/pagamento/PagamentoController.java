2c2589a618cf815c205a2ee714873e425e974ba6
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-05-28 15:29:40 -0300

    201 CREATED e get por id em pagamentos

--- eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
+++ eats-pagamento/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
@@ -1,13 +1,18 @@
 package br.com.caelum.eats.pagamento;
 
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.caelum.eats.exception.ResourceNotFoundException;
 import br.com.caelum.eats.pedido.Pedido;
@@ -24,11 +29,18 @@ class PagamentoController {
 	private PedidoService pedidos;
 	private SimpMessagingTemplate websocket;
 
+	@GetMapping("/{id}")
+	public PagamentoDto detalha(@PathVariable Long id) {
+		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(() -> new ResourceNotFoundException());
+		return new PagamentoDto(pagamento);
+	}
+
 	@PostMapping
-	public PagamentoDto cria(@RequestBody Pagamento pagamento) {
+	public ResponseEntity<PagamentoDto> cria(@RequestBody Pagamento pagamento, UriComponentsBuilder uriBuilder) {
 		pagamento.setStatus(Pagamento.Status.CRIADO);
 		Pagamento salvo = pagamentoRepo.save(pagamento);
-		return new PagamentoDto(salvo);
+		URI path = uriBuilder.path("/pagamentos/{id}").buildAndExpand(salvo.getId()).toUri();
+		return ResponseEntity.created(path).body(new PagamentoDto(salvo));
 	}
 
 	@PutMapping("/{id}")
