5ac4ffcf575c0b8b76742e1e1d24983f189c6aa5
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2019-07-16 15:42:00 -0300

    corrige uso do Lombok que causava BUILD FAILURE no Maven

--- eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/RestauranteService.java
+++ eats-restaurante/src/main/java/br/com/caelum/eats/restaurante/RestauranteService.java
@@ -1,15 +1,30 @@
 package br.com.caelum.eats.restaurante;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import br.com.caelum.eats.admin.TipoDeCozinha;
 import lombok.AllArgsConstructor;
-import lombok.experimental.Delegate;
 
 @Service
 @AllArgsConstructor
 public class RestauranteService {
 
-	@Delegate
 	private RestauranteRepository repo;
+
+	public Page<Restaurante> findAllByAprovado(boolean aprovado, Pageable limit) {
+		return repo.findAllByAprovado(aprovado, limit);
+	}
+
+	public Page<Restaurante> findAllByAprovadoAndTipoDeCozinha(boolean aprovado, TipoDeCozinha tipo, Pageable limit) {
+		return repo.findAllByAprovadoAndTipoDeCozinha(aprovado, tipo, limit);
+	}
+
+	public Optional<Restaurante> findById(Long id) {
+		return repo.findById(id);
+	}
 
 }
